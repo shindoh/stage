65cf4b869a4affa00c2d43b70fe1f87a56ab0369
shindoh <shindoh87@gmail.com>
2013-01-13 23:40:40 +0900

    Update src/com/challenge/stage/StageActivity.java

--- src/com/challenge/stage/StageActivity.java
+++ src/com/challenge/stage/StageActivity.java
@@ -1,24 +1,80 @@
-package com.challenge.stage;
-
-import org.apache.cordova.DroidGap;
-
-import android.os.Bundle;
-import android.view.View;
-
-public class StageActivity extends DroidGap {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/app/main.html");
-
-   
-        super.appView.setVerticalScrollBarEnabled(true);
-        super.appView.setHorizontalScrollBarEnabled(false);
-   
-        super.appView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        
-    }
-    
-    
+public class NotifyService extends Service{
+    private final int NOTI_ID=1;
+	private ConditionVariable condValue;
+	private NotificationManager nm;
+	
+	@Override
+	public void onCreate() {
+		Log.e("onCreate","check");
+		nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+		
+		condValue = new ConditionVariable(false);
+		Thread thread= new Thread(null, run, "notify");
+		thread.start();
+		super.onCreate();
+	}
+	
+	
+	
+	Runnable run = new Runnable() {
+		
+		@Override
+		public void run() {
+			for(int i = 0 ; i < 4 ; i++)
+			{
+				showNotification("테스트 1");
+				Log.e(i+"번째 ","테스트1");
+				if(condValue.block(3*1000))		//pause
+					break;
+				showNotification("테스트 2");
+				Log.e(i+"번째 ","테스트2");
+				if(condValue.block(3*1000))
+					break;
+				showNotification("테스트 3");
+				Log.e(i+"번째 ","테스트3");
+				if(condValue.block(3*1000))
+					break;
+				
+			}
+			
+			NotifyService.this.stopSelf();
+			
+		}
+	};
+	
+	@SuppressLint("NewApi")
+	private void showNotification(String text)
+	{
+		Notification notify = new Notification();
+		notify.icon = R.drawable.ic_launcher;
+		notify.tickerText = text;
+		notify.when = System.currentTimeMillis();
+		PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, new Intent(getApplicationContext(),MainActivity.class), 0);
+		notify.setLatestEventInfo(getApplicationContext(), "테스트", text, pendingIntent);
+		
+		nm.notify(NOTI_ID, notify);
+	}
+	
+	
+	
+	@Override
+	public void onDestroy() {
+		
+		nm.cancel(NOTI_ID);
+		condValue.open();
+		Log.e("onDestroy","check");
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Log.e("onStartCommand","check");
+		return super.onStartCommand(intent, flags, startId);
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		condValue.open();
+		return null;
+	}
 }
+
