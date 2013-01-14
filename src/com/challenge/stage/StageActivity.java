c4858f7ba501ef32dd28fbcca9539ae0f66713a7
shindoh <shindoh87@gmail.com>
2013-01-14 16:53:54 +0900

    Update src/com/challenge/stage/StageActivity.java

--- src/com/challenge/stage/StageActivity.java
+++ src/com/challenge/stage/StageActivity.java
@@ -1,22 +1,21 @@
-	@SuppressLint("NewApi")
+@SuppressLint("NewApi")
 	private void showNotification(String text)
 	{
 		PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, new Intent(getApplicationContext(),MainActivity.class), 0);
 		Notification notify = new Notification();
 		
 		try {
-			Object notifyBuilder = Class.forName("android.app.Notification.Builder").newInstance();
-
+			Object notifyBuilder = Class.forName("android.app.Notification.Builder").getConstructor(new Class[]{Context.class}).newInstance(this);
 			Method setTickerMethod = notifyBuilder.getClass().getMethod("setTicker", new Class[]{CharSequence.class});
+			setTickerMethod.invoke(notifyBuilder, text);
+			
 			Method setSmallIconMethod = notifyBuilder.getClass().getMethod("setSmallIcon", new Class[]{int.class});
 			Method setLargeIconMethod = notifyBuilder.getClass().getMethod("setLargeIcon", new Class[]{int.class});
 			Method setWhenMethod = notifyBuilder.getClass().getMethod("setWhen", new Class[]{long.class});
 			Method setContentTitleMethod = notifyBuilder.getClass().getMethod("setContentTitle", new Class[]{CharSequence.class});
 			Method setContentTextMethod = notifyBuilder.getClass().getMethod("setContentText", new Class[]{CharSequence.class});
 			Method setContentIntentMethod = notifyBuilder.getClass().getMethod("setContentIntent", new Class[]{PendingIntent.class});
-			
-			
-			setTickerMethod.invoke(notifyBuilder, text);
+
 			setSmallIconMethod.invoke(notifyBuilder, R.drawable.ic_launcher);
 			setLargeIconMethod.invoke(notifyBuilder, R.drawable.ic_launcher);
 			setWhenMethod.invoke(notifyBuilder, System.currentTimeMillis());			
